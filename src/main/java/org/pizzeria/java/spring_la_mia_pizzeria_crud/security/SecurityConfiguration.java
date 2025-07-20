2e32dff108f26a7af0030ff8871bc0a38d987d0b
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-07-20 02:47:14 +0200

    add csrf disable

--- src/main/java/org/pizzeria/java/spring_la_mia_pizzeria_crud/security/SecurityConfiguration.java
+++ src/main/java/org/pizzeria/java/spring_la_mia_pizzeria_crud/security/SecurityConfiguration.java
@@ -28,7 +28,8 @@ public class SecurityConfiguration {
                 .requestMatchers("/**").permitAll()
                 .and().formLogin()
                 .and().logout()
-                .and().exceptionHandling();
+                .and().exceptionHandling()
+                .and().csrf().disable();
         return http.build();
     }
 
